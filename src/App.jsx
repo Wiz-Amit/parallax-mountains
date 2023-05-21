af737d73e043cf6a9bedd794715e67d938cff424
Amit Hazra <ahazratwt@gmail.com>
2023-05-21 20:23:09 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import background from "./assets/background.png";
+
+function App() {
+  return (
+    <>
+      <p className="text-red-500 border border-red-500">test</p>
+      {/* <img src={background} className="w-100vw" /> */}
+    </>
+  );
+}
+
+export default App;
